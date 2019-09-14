4a4a7013deef1ccad76b4584947ccb7e8f3975e4
Hunter Travis Klein <hklein@cse.unl.edu>
2019-09-13 19:05:41 -0500

    Fixed Syntax

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -25,7 +25,7 @@
 int isKaprekar(int n) {
 
   if(n < 1) {
-    return false;
+    return 0;
   }
 
   int i;
@@ -33,22 +33,22 @@ int isKaprekar(int n) {
   long square = n * (long) n;
   //compute the number of digits of the square
   int numDigits = (int) log10(n) + 1;
-  long modulus = 0;
+  long modulous = 0;
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
     modulous *= 10;
 
     //split the square into two parts
-    first = square / modulus;
-    second = square % modulus;
+    first = square / modulous;
+    second = square % modulous;
 
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
     //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n = first + second) {
+    if(second > 0 && n == first + second) {
       return 1;
     }
   }
